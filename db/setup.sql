12de2102cf909e9bb998cf034507baa1f8322c6e
alkhe <raphaeljmu@gmail.com>
2021-08-29 23:18:17 +1000

    add basic permissioning

--- db/setup.sql
+++ db/setup.sql
@@ -1,18 +1,22 @@
-drop table if exists subjects;
+drop table if exists subjects cascade;
 
 create table subjects (
   id serial primary key not null,
   subject_id char(6) not null,
-  test_group integer not null,
+  test_group integer,
   first_name varchar(35) not null,
   last_name varchar(35) not null,
-  email varchar(120) not null
+  email varchar(120) not null,
+  secret char(21)
 );
 
 create unique index subjects_subject_id on subjects (subject_id);
 create index subjects_test_group on subjects (test_group);
 
-drop table if exists reports;
+-- null secret means the user is uninducted
+-- test_group to be assigned once all subject surveys collected
+
+drop table if exists reports cascade;
 
 create table reports (
   id serial primary key not null,
@@ -28,4 +32,7 @@ create index reports_subject_id on reports (subject_id);
 create index reports_application_id on reports (application_id);
 
 -- foreground_time and visible_time in seconds
+
+insert into subjects (subject_id, first_name, last_name, email) values ('abcdef', 'Raphael', 'Mu', 'raphaeljmu@gmail.com');
+
 
--- pages/api/api.ts
+++ pages/api/api.ts
@@ -3,6 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import get_client from '../../db/connect'
 import { add_subject, get_subject_by_subject_id, get_subjects, check_id } from '../../db/driver'
 import { generate_id } from '../../src/subject'
+import { nanoid } from 'nanoid'
+
+const admin_token = require('../../admin-token.json').token
 
 async function generate_new_id(client: ClientBase) {
   let id = generate_id()
@@ -33,72 +36,88 @@ async function handle_method(res: NextApiResponse, validate: () => string[], run
   await run()
 }
 
+function bad_request(res: NextApiResponse, code: string, message: string) {
+  res.statusCode = 400
+  res.json({ code, message })
+}
+
+function forbidden(res: NextApiResponse) {
+  res.statusCode = 403
+  res.end()
+}
+
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 
-  const q = req.query
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    return bad_request(res, 'http-unsupported', `HTTP method '${ req.method }' unsupported.`)
+  }
+
+  const q = req.method === 'GET' ? req.query : JSON.parse(req.body)
 
   try {
-    switch (q.method) {
-      case 'add-subject':
-        return await handle_method(
-          res,
-          () => {
-            const errors = []
+    if (q.method === 'initial-auth') {
+      if (q.first_name == null) return bad_request(res, 'missing-field', 'Missing first name.')
+      if (q.subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
 
-            if (q.first_name == null) errors.push('Missing first name.')
-            if (q.last_name == null) errors.push('Missing last name.')
-            if (q.email == null) errors.push('Missing email.')
+      const client = await get_client()
 
-            return errors
-          },
-          async () => {
-            const client = await get_client()
+      const result = await check_id(client, q.subject_id as string)
 
-            const subject_id = await generate_new_id(client)
+      if (result.rows.length === 0 || result.rows[0].first_name !== q.first_name as string) return bad_request(res, 'subject-not-found', 'Subject with that ID and first name combination does not exist.')
 
-            await add_subject(client, subject_id, 0, q.first_name as string, q.last_name as string, q.email as string)
+      const secret = nanoid()
 
-            res.json({ subject_id })
-          }
-        )
-      case 'get-subject':
-        return await handle_method(
-          res,
-          () => {
-            const errors = []
+      // update secret in database
 
-            if (q.subject_id == null) errors.push('Missing subject id.')
+      res.json({ data: { secret } })
+    } else if (q.method === 'add-subject') {
+      if (q.admin_token !== admin_token) return forbidden(res)
 
-            return errors
-          },
-          async () => {
-            const client = await get_client()
+      const errors = []
 
-            const result = await get_subject_by_subject_id(client, q.subject_id as string)
+      if (q.first_name == null) errors.push('Missing first name.')
+      if (q.last_name == null) errors.push('Missing last name.')
+      if (q.email == null) errors.push('Missing email.')
 
-            if (result.rows.length !== 0) {
-              res.json({ data: result.rows[0] })
-            } else {
-              res.json({ data: {} })
-            }
-          }
-        )
-      case 'get-all-subjects':
-        return await handle_method(
-          res,
-          () => [],
-          async () => {
-            const client = await get_client()
+      if (errors.length !== 0) return bad_request(res, 'missing-field', errors.join('\n'))
 
-            const result = await get_subjects(client)
+      const client = await get_client()
 
-            res.json({ data: result.rows })
-          }
-        )
-      default:
-        res.statusCode = 400
-        res.json({ message: 'Unrecognized method.' })
-        return
+      const subject_id = await generate_new_id(client)
+
+      await add_subject(client, subject_id, 0, q.first_name as string, q.last_name as string, q.email as string)
+
+      res.json({ subject_id })
+    } else if (q.method === 'get-subject') {
+      if (q.subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
+
+      const client = await get_client()
+
+      const result = await get_subject_by_subject_id(client, q.subject_id as string)
+
+      if (q.admin_token === admin_token) {
+        if (result.rows.length !== 0) {
+          res.json({ data: result.rows[0] })
+        } else {
+          res.json({ data: {} })
+        }
+      } else {
+        if (result.rows.length !== 0 && result.rows[0].secret === q.secret as string) {
+          res.json({ data: result.rows[0] })
+        } else {
+          return forbidden(res)
+        }
+      }
+    } else if (q.method === 'get-all-subjects') {
+      if (q.admin_token !== admin_token) return forbidden(res)
+
+      const client = await get_client()
+
+      const result = await get_subjects(client)
+
+      res.json({ data: result.rows })
+    } else {
+      return bad_request(res, 'unsupported-method', 'Unsupported method.')
     }
   } catch (err) {
     res.statusCode = 500
