718e8cb8278b6698dd4c0f4481db1f8b353cf1d5
Zhuoling Zhou <16379978+ZhouZ-1@users.noreply.github.com>
2022-06-02 15:14:52 +1000

    Configure CORS for Next.js server

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
