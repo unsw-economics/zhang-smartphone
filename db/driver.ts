3a28a3a7514181a715f784fba522564aea17d3a7
Zhuoling Zhou <16379978+ZhouZ-1@users.noreply.github.com>
2022-06-23 16:54:47 +1000

    Remove requirement for study_group parameter

--- db/driver.ts
+++ db/driver.ts
@@ -153,5 +153,5 @@ export function get_all_usage(client: ClientBase) {
 }
 
 export function get_usage_summary(client: ClientBase, subject_id: string) {
-  return client.query('select * from summary_view where study_group=?', [subject_id])
+  return client.query('select * from summary_view', [subject_id])
 }
