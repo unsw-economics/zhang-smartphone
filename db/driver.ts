7044dc9618227f3e7c3a76a85a9f90846b0afb6c
Zhuoling Zhou <16379978+ZhouZ-1@users.noreply.github.com>
2022-06-27 22:26:42 +1000

    Run prettier over everything

--- db/driver.ts
+++ db/driver.ts
@@ -1,23 +1,37 @@
-import { ClientBase, QueryResult } from 'pg'
+import { ClientBase, QueryResult } from "pg";
 
 // subjects
 
-export function add_subject(client: ClientBase, subject_id: string, email: string, secret: string) {
+export function add_subject(
+  client: ClientBase,
+  subject_id: string,
+  email: string,
+  secret: string
+) {
   return client.query(
-    'insert into subjects (subject_id, email, secret, test_group) values ($1, $2, $3, 0)',
+    "insert into subjects (subject_id, email, secret, test_group) values ($1, $2, $3, 0)",
     [subject_id, email, secret]
-  )
+  );
 }
 
-export function update_subject_id(client: ClientBase, subject_id: string, email: string) {
-  return client.query(
-    'update subjects set subject_id = $1 where email = $2',
-    [subject_id, email]
-  )
+export function update_subject_id(
+  client: ClientBase,
+  subject_id: string,
+  email: string
+) {
+  return client.query("update subjects set subject_id = $1 where email = $2", [
+    subject_id,
+    email,
+  ]);
 }
 
-export function get_subject_by_subject_id(client: ClientBase, subject_id: string) {
-  return client.query('select * from subjects where subject_id = $1', [subject_id])
+export function get_subject_by_subject_id(
+  client: ClientBase,
+  subject_id: string
+) {
+  return client.query("select * from subjects where subject_id = $1", [
+    subject_id,
+  ]);
 }
 
 export function get_subjects(client: ClientBase, group?: string) {
@@ -30,134 +44,230 @@ export function get_subjects(client: ClientBase, group?: string) {
   return client.query("select * from subjects_view");
 }
 
-function set_subject_field<T = string>(client: ClientBase, field: string, subject_id: string, value: T) {
+function set_subject_field<T = string>(
+  client: ClientBase,
+  field: string,
+  subject_id: string,
+  value: T
+) {
   return client.query(
-    `update subjects set ${ field } = $1 where subject_id = $2`,
+    `update subjects set ${field} = $1 where subject_id = $2`,
     [value, subject_id]
-  )
+  );
 }
 
-function check_subject_fields<T>(client: ClientBase, subject_id: string, fields: string[]): Promise<QueryResult<T>> {
-  return client.query<T>(`select ${ fields.join(', ') } from subjects where subject_id = $1`, [subject_id])
+function check_subject_fields<T>(
+  client: ClientBase,
+  subject_id: string,
+  fields: string[]
+): Promise<QueryResult<T>> {
+  return client.query<T>(
+    `select ${fields.join(", ")} from subjects where subject_id = $1`,
+    [subject_id]
+  );
 }
 
-export function set_test_group(client: ClientBase, subject_id: string, test_group: number) {
-  return set_subject_field<number>(client, 'test_group', subject_id, test_group)
+export function set_test_group(
+  client: ClientBase,
+  subject_id: string,
+  test_group: number
+) {
+  return set_subject_field<number>(
+    client,
+    "test_group",
+    subject_id,
+    test_group
+  );
 }
 
-type IDGroupPair = [string, number]
+type IDGroupPair = [string, number];
 
-export function set_treatment_limit(client: ClientBase, subject_id: string, treatment_limit: number) {
-  return set_subject_field<number>(client, 'treatment_limit', subject_id, treatment_limit)
+export function set_treatment_limit(
+  client: ClientBase,
+  subject_id: string,
+  treatment_limit: number
+) {
+  return set_subject_field<number>(
+    client,
+    "treatment_limit",
+    subject_id,
+    treatment_limit
+  );
 }
 
 export function set_identified(client: ClientBase, subject_id: string) {
-  return set_subject_field<boolean>(client, 'identified', subject_id, true)
+  return set_subject_field<boolean>(client, "identified", subject_id, true);
 }
 
-function set_fields_on_subjects<T extends any[]>(client: ClientBase, fields: string[], updates: [string, ...T][], options?: ExpandOptions ) {
-  const field_setters = fields.map(f => `${ f } = t.${ f }`).join(', ')
-  const table = expand_args(updates.length, fields.length + 1, options)
+function set_fields_on_subjects<T extends any[]>(
+  client: ClientBase,
+  fields: string[],
+  updates: [string, ...T][],
+  options?: ExpandOptions
+) {
+  const field_setters = fields.map((f) => `${f} = t.${f}`).join(", ");
+  const table = expand_args(updates.length, fields.length + 1, options);
 
   return client.query(
-    `update subjects as s set ${ field_setters } from (values ${ table }) as t(subject_id, ${ fields.join(', ') }) where s.subject_id = t.subject_id`,
+    `update subjects as s set ${field_setters} from (values ${table}) as t(subject_id, ${fields.join(
+      ", "
+    )}) where s.subject_id = t.subject_id`,
     updates.flat()
-  )
+  );
 }
 
-export function set_test_params(client: ClientBase, updates: [string, number, number | null, number][]) {
-  return set_fields_on_subjects<[number, number | null, number]>(client, ['test_group', 'treatment_intensity', 'treatment_limit'], updates, { types: [null, 'int', 'int', 'int'] })
+export function set_test_params(
+  client: ClientBase,
+  updates: [string, number, number | null, number][]
+) {
+  return set_fields_on_subjects<[number, number | null, number]>(
+    client,
+    ["test_group", "treatment_intensity", "treatment_limit"],
+    updates,
+    { types: [null, "int", "int", "int"] }
+  );
 }
 
 export function check_id(client: ClientBase, subject_id: string) {
-  return check_subject_fields<{ 'subject_id': string }>(client, subject_id, ['subject_id'])
+  return check_subject_fields<{ subject_id: string }>(client, subject_id, [
+    "subject_id",
+  ]);
 }
 
 export function check_secret(client: ClientBase, subject_id: string) {
-  return check_subject_fields<{ 'secret': string }>(client, subject_id, ['secret'])
+  return check_subject_fields<{ secret: string }>(client, subject_id, [
+    "secret",
+  ]);
 }
 
-export async function email_exists(client: ClientBase, email: string): Promise<boolean> {
-  return (await client.query<{ 'count': number }>(`select count(*) from subjects where email = '${ email }'`)).rows[0].count > 0
+export async function email_exists(
+  client: ClientBase,
+  email: string
+): Promise<boolean> {
+  return (
+    (
+      await client.query<{ count: number }>(
+        `select count(*) from subjects where email = '${email}'`
+      )
+    ).rows[0].count > 0
+  );
 }
 
 // reports
 
-export type DBReport = [string, string, string, number, number]
+export type DBReport = [string, string, string, number, number];
 
 export function get_reports(client: ClientBase) {
-  return client.query('select * from reports')
+  return client.query("select * from reports");
 }
 
 export function add_reports(client: ClientBase, app_reports: DBReport[]) {
   return client.query(
-    `insert into reports (subject_id, application_name, period, day, usage) values ${ expand_args(app_reports.length, 5) } on conflict do nothing`,
+    `insert into reports (subject_id, application_name, period, day, usage) values ${expand_args(
+      app_reports.length,
+      5
+    )} on conflict do nothing`,
     app_reports.flat()
-  )
+  );
 }
 
-export async function get_study_dates(client: ClientBase, subject_id: string): Promise<{baseline_date: string; treatment_date: string; endline_date: string; over_date: string;}> {
-  return (await client.query(
-    `select baseline_date::text, treatment_date::text, endline_date::text, over_date::text from subjects s join study_dates d on s.study_group = d.period_name where s.subject_id = $1`,
-    [subject_id])).rows[0] || {baseline_date: null, treatment_date: null, endline_date: null, over_date: null}
+export async function get_study_dates(
+  client: ClientBase,
+  subject_id: string
+): Promise<{
+  baseline_date: string;
+  treatment_date: string;
+  endline_date: string;
+  over_date: string;
+}> {
+  return (
+    (
+      await client.query(
+        `select baseline_date::text, treatment_date::text, endline_date::text, over_date::text from subjects s join study_dates d on s.study_group = d.period_name where s.subject_id = $1`,
+        [subject_id]
+      )
+    ).rows[0] || {
+      baseline_date: null,
+      treatment_date: null,
+      endline_date: null,
+      over_date: null,
+    }
+  );
 }
 
 type ExpandOptions = {
-  types?: (string | null)[],
-  start_index?: number
-}
+  types?: (string | null)[];
+  start_index?: number;
+};
 
-function expand_args(rows: number, columns: number, options?: ExpandOptions): string {
-  const params = []
+function expand_args(
+  rows: number,
+  columns: number,
+  options?: ExpandOptions
+): string {
+  const params = [];
 
-  const { types, start_index } = { types: [], start_index: 1, ...options }
+  const { types, start_index } = { types: [], start_index: 1, ...options };
 
   for (let i = 0; i < rows; i++) {
-    const tokens = []
+    const tokens = [];
 
     for (let j = 0; j < columns; j++) {
-      const base = `$${ i * columns + j + start_index }`
-      tokens.push(
-        types[j] == null
-          ? base
-          : `${ base }::${ types[j] }`
-      )
+      const base = `$${i * columns + j + start_index}`;
+      tokens.push(types[j] == null ? base : `${base}::${types[j]}`);
     }
 
-    params.push(`(${ tokens.join(',') })`)
+    params.push(`(${tokens.join(",")})`);
   }
 
-  return params.join(',')
+  return params.join(",");
 }
 
 // crash reports
 
-export function add_crash_report(client: ClientBase, subject_id: string | null, report: string) {
-  return client.query('insert into crash_reports (subject_id, report) values ($1, $2)', [subject_id, report])
+export function add_crash_report(
+  client: ClientBase,
+  subject_id: string | null,
+  report: string
+) {
+  return client.query(
+    "insert into crash_reports (subject_id, report) values ($1, $2)",
+    [subject_id, report]
+  );
 }
 
 interface usageHash {
-  [key:string]: number;
+  [key: string]: number;
 }
 
-export function add_usage(client: ClientBase, subject_id: string, usage: usageHash) {
-  
-  const usage_array = Object.keys(usage).map((key) => [subject_id, key, usage[key]])
+export function add_usage(
+  client: ClientBase,
+  subject_id: string,
+  usage: usageHash
+) {
+  const usage_array = Object.keys(usage).map((key) => [
+    subject_id,
+    key,
+    usage[key],
+  ]);
 
   return client.query(
-    `insert into usage_backup (subject_id, date_reported, usage) values ${ expand_args(Object.keys(usage).length, 3) }`,
+    `insert into usage_backup (subject_id, date_reported, usage) values ${expand_args(
+      Object.keys(usage).length,
+      3
+    )}`,
     usage_array.flat()
-  )
+  );
 }
 
 export function get_all_study_dates(client: ClientBase) {
-  return client.query('select * from study_dates')
+  return client.query("select * from study_dates");
 }
 
 export function get_all_usage(client: ClientBase) {
-  return client.query('select * from usage_view')
+  return client.query("select * from usage_view");
 }
 
 export function get_usage_summary(client: ClientBase) {
-  return client.query('select * from summary_view')
+  return client.query("select * from summary_view");
 }
--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,308 +1,342 @@
-import { ClientBase, DatabaseError } from 'pg'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { ClientBase, DatabaseError } from "pg";
+import { NextApiRequest, NextApiResponse } from "next";
 import {
-  add_subject, update_subject_id, get_subject_by_subject_id, get_subjects, set_identified, check_id, check_secret, email_exists, set_test_params,
-  get_reports, add_reports,
+  add_subject,
+  update_subject_id,
+  get_subject_by_subject_id,
+  get_subjects,
+  set_identified,
+  check_id,
+  check_secret,
+  email_exists,
+  set_test_params,
+  get_reports,
+  add_reports,
   add_crash_report,
   DBReport,
   get_study_dates,
   add_usage,
   get_all_study_dates,
   get_all_usage,
-  get_usage_summary
-} from '../db/driver'
-import { generate_id } from './subject'
-import { bad_request, forbidden, ok } from './response'
-import { nanoid } from 'nanoid'
-import { writeFileSync } from 'fs'
+  get_usage_summary,
+} from "../db/driver";
+import { generate_id } from "./subject";
+import { bad_request, forbidden, ok } from "./response";
+import { nanoid } from "nanoid";
+import { writeFileSync } from "fs";
 
 export type EndpointExtra = {
-  method: string
-  auth_token?: string
-  client: ClientBase
-}
+  method: string;
+  auth_token?: string;
+  client: ClientBase;
+};
 
-export type Endpoint = (req: NextApiRequest, res: NextApiResponse, extra: EndpointExtra) => Promise<void>
+export type Endpoint = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  extra: EndpointExtra
+) => Promise<void>;
 
 export type EndpointCarrier = {
-  [k: string]: Endpoint
-}
+  [k: string]: Endpoint;
+};
 
-const admin_token = process.env.ADMIN_TOKEN
-const qualtrics_token = process.env.QUALTRICS_TOKEN
+const admin_token = process.env.ADMIN_TOKEN;
+const qualtrics_token = process.env.QUALTRICS_TOKEN;
 
 type RawReport = {
-  application_name: string
-  usage: number
-}
+  application_name: string;
+  usage: number;
+};
 
 async function generate_new_id(client: ClientBase) {
-  let id = generate_id()
-  let ready = false
+  let id = generate_id();
+  let ready = false;
 
   while (!ready) {
-    const result = await check_id(client, id)
+    const result = await check_id(client, id);
 
     if (result.rows.length === 0) {
-      ready = true
+      ready = true;
     } else {
-      id = generate_id()
+      id = generate_id();
     }
   }
 
-  return id
+  return id;
 }
 
 function http_get(endpoint: Endpoint): Endpoint {
-  return async function(req, res, extra) {
-
-    if(extra.method === 'OPTIONS') {
-      return ok(res)
+  return async function (req, res, extra) {
+    if (extra.method === "OPTIONS") {
+      return ok(res);
     }
-    
-    if (extra.method !== 'GET') return bad_request(
-      res,
-      'wrong-method',
-      'This endpoint accepts GET requests only.'
-    )
 
-    return endpoint(req, res, extra)
-  }
+    if (extra.method !== "GET")
+      return bad_request(
+        res,
+        "wrong-method",
+        "This endpoint accepts GET requests only."
+      );
+
+    return endpoint(req, res, extra);
+  };
 }
 
 function http_post(endpoint: Endpoint): Endpoint {
-  return async function(req, res, extra) {
-
-    if(extra.method === 'OPTIONS') {
-      return ok(res)
+  return async function (req, res, extra) {
+    if (extra.method === "OPTIONS") {
+      return ok(res);
     }
 
-    if (extra.method !== 'POST') return bad_request(
-      res,
-      'wrong-method',
-      'This endpoint accepts POST requests only.'
-    )
+    if (extra.method !== "POST")
+      return bad_request(
+        res,
+        "wrong-method",
+        "This endpoint accepts POST requests only."
+      );
 
-    return endpoint(req, res, extra)
-  }
+    return endpoint(req, res, extra);
+  };
 }
 
 const endpoints: EndpointCarrier = {
   identify: http_post(async (req, res, { client }) => {
-    const { subject_id } = req.body
+    const { subject_id } = req.body;
 
-    if (subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
+    if (subject_id == null)
+      return bad_request(res, "missing-field", "Missing subject ID.");
 
-    const result = await get_subject_by_subject_id(client, subject_id)
+    const result = await get_subject_by_subject_id(client, subject_id);
 
-    if (result.rows.length === 0) return bad_request(
-      res,
-      'subject-not-found',
-      'Subject with that ID does not exist.'
-    )
+    if (result.rows.length === 0)
+      return bad_request(
+        res,
+        "subject-not-found",
+        "Subject with that ID does not exist."
+      );
 
-    const subject = result.rows[0]
+    const subject = result.rows[0];
 
     if (!subject.identified) {
-      await set_identified(client, subject_id)
-      req.log.info(`subject ${ subject_id } identified`)
+      await set_identified(client, subject_id);
+      req.log.info(`subject ${subject_id} identified`);
     } else {
-      req.log.info(`subject ${ subject_id } identified again`)
+      req.log.info(`subject ${subject_id} identified again`);
     }
 
     res.json({
       data: {
-        auth_token: subject.secret
-      }
-    })
+        auth_token: subject.secret,
+      },
+    });
   }),
 
-  'get-test-params': http_get(async (req, res, { auth_token, client }) => {
-    const { subject_id } = req.query as Record<string, string>
+  "get-test-params": http_get(async (req, res, { auth_token, client }) => {
+    const { subject_id } = req.query as Record<string, string>;
 
-    const result = await get_subject_by_subject_id(client, subject_id)
+    const result = await get_subject_by_subject_id(client, subject_id);
 
     if (
       auth_token === admin_token ||
-      result.rows.length !== 0 && auth_token === result.rows[0].secret
+      (result.rows.length !== 0 && auth_token === result.rows[0].secret)
     ) {
-      const subject = result.rows[0]
+      const subject = result.rows[0];
 
       res.json({
         data: {
           test_group: subject.test_group,
           treatment_intensity: subject.treatment_intensity,
-          treatment_limit: subject.treatment_limit
-        }
-      })
+          treatment_limit: subject.treatment_limit,
+        },
+      });
     } else {
-      return forbidden(res)
+      return forbidden(res);
     }
   }),
 
-  'set-test-params': http_post(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "set-test-params": http_post(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
-    const updates = req.body
+    const updates = req.body;
 
-    if (updates == null || updates.constructor !== Array) return bad_request(res, 'incorrect-format', 'Expecting array of [subject_id, test_group, treatment_limit] triples.')
+    if (updates == null || updates.constructor !== Array)
+      return bad_request(
+        res,
+        "incorrect-format",
+        "Expecting array of [subject_id, test_group, treatment_limit] triples."
+      );
 
-    await set_test_params(client, updates)
+    await set_test_params(client, updates);
 
-    req.log.info(`update ${ updates.map(p => `(${ p.join(', ') })`).join(', ') }`)
+    req.log.info(
+      `update ${updates.map((p) => `(${p.join(", ")})`).join(", ")}`
+    );
 
-    res.json({})
+    res.json({});
   }),
 
-  'add-subject': http_post(async (req, res, { auth_token, client }) => {
-      if (auth_token !== qualtrics_token && auth_token !== admin_token) return forbidden(res)
+  "add-subject": http_post(async (req, res, { auth_token, client }) => {
+    if (auth_token !== qualtrics_token && auth_token !== admin_token)
+      return forbidden(res);
 
-      const { subject_id, email } = req.body
+    const { subject_id, email } = req.body;
 
-      if (subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
-      if (email == null) return bad_request(res, 'missing-field', 'Missing email.')
+    if (subject_id == null)
+      return bad_request(res, "missing-field", "Missing subject ID.");
+    if (email == null)
+      return bad_request(res, "missing-field", "Missing email.");
 
-      if (await email_exists(client, email)) {
-        await update_subject_id(client, subject_id, email)
-      } else {
-        await add_subject(client, subject_id, email, nanoid())
-      }
+    if (await email_exists(client, email)) {
+      await update_subject_id(client, subject_id, email);
+    } else {
+      await add_subject(client, subject_id, email, nanoid());
+    }
 
-      res.json({
-        data: { subject_id }
-      })
+    res.json({
+      data: { subject_id },
+    });
   }),
 
-  'get-subject': http_get(async (req, res, { auth_token, client }) => {
-    const { subject_id } = req.query as Record<string, string>
+  "get-subject": http_get(async (req, res, { auth_token, client }) => {
+    const { subject_id } = req.query as Record<string, string>;
 
-    if (subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
+    if (subject_id == null)
+      return bad_request(res, "missing-field", "Missing subject ID.");
 
-    const result = await get_subject_by_subject_id(client, subject_id)
+    const result = await get_subject_by_subject_id(client, subject_id);
 
     if (auth_token === admin_token) {
       if (result.rows.length !== 0) {
-        res.json({ data: result.rows[0] })
+        res.json({ data: result.rows[0] });
       } else {
-        res.json({})
+        res.json({});
       }
     } else {
       if (result.rows.length !== 0 && auth_token === result.rows[0].secret) {
-        res.json({ data: result.rows[0] })
+        res.json({ data: result.rows[0] });
       } else {
-        return forbidden(res)
+        return forbidden(res);
       }
     }
   }),
 
-  'get-all-subjects': http_get(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "get-all-subjects": http_get(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
     const { group } = req.query as Record<string, string | undefined>;
 
-    const result = await get_subjects(client, group)
+    const result = await get_subjects(client, group);
 
-    res.json({ data: result.rows })
+    res.json({ data: result.rows });
   }),
 
-  'submit-report': http_post(async (req, res, { auth_token, client }) => {
-    const { subject_id, period, day, reports } = req.body
+  "submit-report": http_post(async (req, res, { auth_token, client }) => {
+    const { subject_id, period, day, reports } = req.body;
 
-    if (subject_id == null) return bad_request(res, 'missing-field', 'Missing subject ID.')
-    if (period == null) return bad_request(res, 'missing-field', 'Missing report period.')
-    if (day == null) return bad_request(res, 'missing-field', 'Missing report day.')
+    if (subject_id == null)
+      return bad_request(res, "missing-field", "Missing subject ID.");
+    if (period == null)
+      return bad_request(res, "missing-field", "Missing report period.");
+    if (day == null)
+      return bad_request(res, "missing-field", "Missing report day.");
 
     if (auth_token !== admin_token) {
-      const result = await check_secret(client, subject_id)
+      const result = await check_secret(client, subject_id);
 
-      if (result.rows.length === 0 || auth_token !== result.rows[0].secret) return forbidden(res)
+      if (result.rows.length === 0 || auth_token !== result.rows[0].secret)
+        return forbidden(res);
     }
 
-    const raw_reports = reports as RawReport[]
+    const raw_reports = reports as RawReport[];
 
-    const db_reports: DBReport[] = raw_reports.map(
-      r => [subject_id, r.application_name, period, day, r.usage]
-    )
+    const db_reports: DBReport[] = raw_reports.map((r) => [
+      subject_id,
+      r.application_name,
+      period,
+      day,
+      r.usage,
+    ]);
 
-    await add_reports(client, db_reports)
+    await add_reports(client, db_reports);
 
     // we use `on conflict do nothing`, so the query will succeed even if there are duplicates
 
     try {
-      await add_reports(client, db_reports)
+      await add_reports(client, db_reports);
     } catch (err) {
-      return bad_request(
-        res,
-        'database-error',
-        (err as DatabaseError).message
-      )
+      return bad_request(res, "database-error", (err as DatabaseError).message);
 
-      throw err
+      throw err;
     }
 
-    res.json({})
+    res.json({});
   }),
 
-  'get-all-reports': http_get(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "get-all-reports": http_get(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
-    const result = await get_reports(client)
+    const result = await get_reports(client);
 
-    res.json({ data: result.rows })
+    res.json({ data: result.rows });
   }),
 
-  'acra': http_post(async (req, res, { auth_token, client }) => {
-    await add_crash_report(client, req.body.CUSTOM_DATA.SUBJECT_ID || null, JSON.stringify(req.body))
+  acra: http_post(async (req, res, { auth_token, client }) => {
+    await add_crash_report(
+      client,
+      req.body.CUSTOM_DATA.SUBJECT_ID || null,
+      JSON.stringify(req.body)
+    );
 
-    res.json({})
+    res.json({});
   }),
 
-  'get-dates': http_get(async (req, res, { auth_token, client }) => {
-    const { subject_id } = req.query as Record<string, string>
+  "get-dates": http_get(async (req, res, { auth_token, client }) => {
+    const { subject_id } = req.query as Record<string, string>;
 
-    const studyDates = await get_study_dates(client, subject_id)
+    const studyDates = await get_study_dates(client, subject_id);
 
-    res.json({data: studyDates})
+    res.json({ data: studyDates });
   }),
 
-  'submit-usage': http_post(async (req, res, { auth_token, client }) => {
-    const { subject_id, usage } = req.body
+  "submit-usage": http_post(async (req, res, { auth_token, client }) => {
+    const { subject_id, usage } = req.body;
 
     if (auth_token !== admin_token) {
-      const result = await check_secret(client, subject_id)
+      const result = await check_secret(client, subject_id);
 
-      if (result.rows.length === 0 || auth_token !== result.rows[0].secret) return forbidden(res)
+      if (result.rows.length === 0 || auth_token !== result.rows[0].secret)
+        return forbidden(res);
     }
 
-    await add_usage(client, subject_id, usage)
+    await add_usage(client, subject_id, usage);
 
-    res.json({})
+    res.json({});
   }),
 
-  'get-all-dates': http_get(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "get-all-dates": http_get(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
-    const result = await get_all_study_dates(client)
-  
-    res.json({ data: result.rows })
+    const result = await get_all_study_dates(client);
+
+    res.json({ data: result.rows });
   }),
 
-  'get-all-usage': http_get(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "get-all-usage": http_get(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
-    const result = await get_all_usage(client)
+    const result = await get_all_usage(client);
 
-    res.json({ data: result.rows })
+    res.json({ data: result.rows });
   }),
 
-  'get-usage-summary': http_get(async (req, res, { auth_token, client }) => {
-    if (auth_token !== admin_token) return forbidden(res)
+  "get-usage-summary": http_get(async (req, res, { auth_token, client }) => {
+    if (auth_token !== admin_token) return forbidden(res);
 
-    const result = await get_usage_summary(client)
+    const result = await get_usage_summary(client);
 
-    res.json({ data: result.rows })
+    res.json({ data: result.rows });
   }),
-}
+};
 
-export default endpoints
-
+export default endpoints;
